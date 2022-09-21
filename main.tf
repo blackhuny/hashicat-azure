6484a2c141a98cb42ab6690072c516136dbf5a80
jh.oh <jh.oh@cloudnetworks.co.kr>
2022-09-21 08:00:30 +0000

    Added network module

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ resource "azurerm_resource_group" "myresourcegroup" {
 
   tags = {
     environment = "Production"
-    #Department  = "devops"
+
   }
 }
 
@@ -112,7 +112,7 @@ resource "azurerm_virtual_machine" "catapp" {
   network_interface_ids         = [azurerm_network_interface.catapp-nic.id]
   delete_os_disk_on_termination = "true"
 
-  Department = "devops"
+
 
   storage_image_reference {
     publisher = var.image_publisher
